d713b01a51754116fdd41219c34a2363ab125432
ntropish <ntropish@gmail.com>
2019-09-15 09:16:59 -0500

    wip commit, primary watcher use case complete, about to create watch function

--- sns.js
+++ sns.js
@@ -6,36 +6,41 @@ const scopeToSns = new WeakMap()
 
 export default function sns(fn) {
   const createSnsScope = fn => scopeHandle => {
-    const snsHandle = new Proxy(() => {}, {
-      get(target, prop, receiver) {
-        if (prop === 'output') {
-          return scopeHandle.output
-        } else if (prop === 'set') {
-          return value => ({
-            [set]: true,
-            value,
-          })
-        } else {
-          return scopeHandle.read(prop).value
-        }
+    const snsHandle = new Proxy(
+      {},
+      {
+        get(target, prop, receiver) {
+          if (prop === 'output') {
+            return scopeHandle.output
+          } else if (prop === 'set') {
+            return value => ({
+              [set]: true,
+              value,
+            })
+            // TODO: Add prefix to built in props
+          } else if (prop === 'subscribe') {
+            return (prop, handler) => {
+              const unsubscribe = scopeHandle.read(prop).subscribe(handler)
+              return unsubscribe
+            }
+          } else {
+            const scope = scopeHandle.read(prop)
+            return scope.value
+          }
+        },
+        set(target, prop, value, receiver) {
+          if (prop === 'input') {
+            scopeHandle.setInput(value)
+          } else if (typeof value === 'object' && value[set]) {
+            const readProp = scopeHandle.read(prop)
+            readProp.value = value.value
+          } else {
+            scopeHandle.scope(prop, createSnsScope(value))
+          }
+          return true
+        },
       },
-      set(target, prop, value, receiver) {
-        if (prop === 'input') {
-          scopeHandle.setInput(value)
-        } else if (typeof value === 'object' && value[set]) {
-          const readProp = scopeHandle.read(prop)
-          readProp.value = value.value
-        } else {
-          scopeHandle.scope(prop, createSnsScope(value))
-        }
-        return true
-      },
-      apply(target, thisArg, [prop, handler]) {
-        const unsubscribe = scopeHandle.read(prop).subscribe(handler)
-        // split for clarity
-        return unsubscribe
-      },
-    })
+    )
     return fn(snsHandle)
   }
 
--- sns.test.js
+++ sns.test.js
@@ -2,34 +2,82 @@ import sns from './sns.js'
 
 const outputs = []
 sns(async function main($) {
+  // con defines constants on the scope
   $.con = function conDef($) {
     return function con(value) {
-      return function conDefiner($) {
+      return $ => {
+        $.input = () => console.error('cannot change a constant')
         return value
       }
     }
   }
-  $.mut = function mutDef($) {
-    return function mut(value) {
-      return function mutDefiner($) {
-        $.input = $.output
-        return value
-      }
+
+  // mut defines mutables
+  $.mut = $.con(function mut(value) {
+    return $ => {
+      $.input = $.output
+      return value
     }
-  }
+  })
+
   $.log = $.con(console.log.bind(console))
-  $.a = $ => 'a string 1'
+
+  // $.watch = $.con(function watch(fn) {
+  //   return
+  // })
+
   $.mutable = $.mut(0)
-  $.b = function bDef() {
+
+  $.watcher = $ => {
+    let reads = {}
+    const fn = $ => {
+      $.log($.mutable)
+    }
+    // skimmer is to intercept all "get"s during fn run
+    const skimmer = new Proxy($, {
+      get(target, prop, receiver) {
+        const value = $[prop]
+        reads[prop] = { value }
+        return value
+      },
+    })
+    // this is called to initialize or rerun the watch
+    const run = () => {
+      // clear all data
+      Object.values(reads).forEach(read => read.unsubscribe())
+      reads = {}
+
+      fn(skimmer)
+
+      // wait to subscribe until after so it doesn't re-trigger
+      Object.entries(reads).forEach(([prop, read]) => {
+        reads[prop].unsubscribe = $.subscribe(prop, (newValue, oldValue) => {
+          if (newValue !== read.value) {
+            run()
+          }
+        })
+      })
+
+      console.log('run complete - watching:', reads)
+    }
+    run()
+  }
+
+  $.a = $ => 'a string 1'
+  $.b = function defineB($) {
+    // setting to a property creates a new sub-scope
     $.a = $ => 'a string 2'
     $.c = $ => {
       $.a = $ => 'a string 5'
-      $('mutable', (a, b) => {
+
+      $.subscribe('mutable', (a, b) => {
+        $.log($.a)
         $.log(a)
       })
     }
   }
-  await new Promise(r => setTimeout(r, 5000))
+
+  // TODO: change set to mutate
   $.mutable = $.set(1)
   $.mutable = $.set(2)
 })
